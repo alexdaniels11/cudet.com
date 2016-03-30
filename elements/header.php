bde1873f02376b795fe875284a448caf20593f59
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-03-30 10:08:50 -0400

    verify email

--- elements/header.php
+++ elements/header.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-	<meta name="google-site-verification" content="z1Dp4mK4FOCdacAQ0lfUN0u9m5UqK9ZivyWMCTvkHuU" />
 	<link rel="icon" type="image/png" href="img/favicon.png">
 	<title>Coding UX Design Efficiency Trainging | CUDET Academy</title>
 	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans:400,300,100,600,700,400italic' rel='stylesheet' type='text/css'>
