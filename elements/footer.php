a1e150dfc46a22fbabd6f4c2ea4387d73f82792e
Alex Daniels <alexanderlevidaniels@gmail.com>
2016-04-11 11:34:13 -0400

    ga code

--- elements/footer.php
+++ elements/footer.php
@@ -2,6 +2,21 @@
 	<p align="center"> &copy; <?php echo Date('Y') ;?> CUDET Academy</p>
 </footer>
 
+<!-- GOOGLE ANALYTICS SCRIPT - BEGIN -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-76223668-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+<!-- GOOGLE ANALYTICS SCRIPT - END -->
+
 <script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
 <script type="text/javascript" src="js/main.js"></script>
 </body>
